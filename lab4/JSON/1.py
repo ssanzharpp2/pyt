c3d880fb3bbf9ea786a296a2bffc10ea43556162
Sanzhar <s_sarkyt@kbtu.kz>
2025-02-14 19:41:16 +0500

    python

--- lab4/JSON/1.py
+++ lab4/JSON/1.py
@@ -1,4 +1,12 @@
 import json
 
-with open("sample-data.json") as f:
+with open("C:/Users/Admin/Downloads/sample-data.json", "r") as f:
     a = json.load(f)
+print("Interface Status")
+print("="*80)
+print("DN", " "*42,"DESCRIPTION", " "*5, "SPEED", " "*4, "MTU")
+print("-"*45, '-'*17, '-'*9, '-'*6)
+for x in a["imdata"]:
+    print(x['l1PhysIf']['attributes']["dn"], end="   "*8)
+    print(x['l1PhysIf']['attributes']['speed'], end="   ")
+    print(x['l1PhysIf']['attributes']['mtu'])
