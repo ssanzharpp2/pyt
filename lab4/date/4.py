248c205e8b26bc64a011633aec65d5af5acda14f
Sanzhar <s_sarkyt@kbtu.kz>
2025-02-15 09:28:47 +0500

    python

--- lab4/date/4.py
+++ lab4/date/4.py
@@ -1,9 +1,20 @@
 import datetime
 
-date1 = datetime.datetime(2025, 1, 24, 12, 0, 0)
-date2 = datetime.datetime(2025, 2, 14, 18, 0, 0)
+n = 2
+while n > 0:
+    y = int(input("Year: "))
+    m = int(input("Month: "))
+    d = int(input("Day: "))
+    h = int(input("Hour: "))
+    m1 = int(input("Minute: "))
+    s = int(input("Second: "))
+    global date1, date2
+    if n == 2:
+        date1 = datetime.datetime(y, m, d, h, m1, s)
+    else:
+        date2 = datetime.datetime(y, m, d, h,m1,s)
+    n -= 1
 dif = date2 - date1
 dfs = dif.total_seconds()
 print(dfs)
-
 
